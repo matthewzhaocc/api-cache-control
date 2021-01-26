1a62a2c9b8d86d5a8c9ffc65ca6a75761ac3152d
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-01-26 14:28:09 -0800

    some code because I am sad

---
+++ app.go
@@ -0,0 +1,107 @@
+// Web application daemon
+package main
+
+import (
+	"fmt"
+	// eh well its a http server so ofc I need a HTTP server library
+	"net/http"
+	"os"
+	"strconv"
+
+	// pull in gorm
+	"gorm.io/gorm"
+	// only allow postgresql because im a masochist
+	"gorm.io/driver/postgres"
+	// gorm prometheus exporter
+	gormPrometheus "gorm.io/plugin/prometheus"
+	// logger because I am not crazy
+	log "github.com/sirupsen/logrus"
+	// prometheus because im hardcore
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
+	"github.com/slok/go-http-metrics/middleware"
+	"github.com/slok/go-http-metrics/middleware/std"
+)
+
+// Person a sql table xd
+type Person struct {
+	gorm.Model
+	UID  uint
+	Name string
+	Age  int
+}
+
+var (
+	db gorm.DB
+)
+// NewUser makes a new User from a POST request
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	// only allow http POST requests
+	if r.Method == "POST" {
+		// parse through the form
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadGateway)
+			return
+		}
+		// Parse age and name from request form
+		Name := r.FormValue("USERNAME")
+		Age := r.FormValue("AGE")
+		// check if we actually got a value
+		if Name == "" || Age == "" {
+			http.Error(w, "DID NOT RECEIVE A FIELD", http.StatusBadRequest)
+			return
+		}
+		// err convert string to int
+		AgeInt, err := strconv.Atoi(Age)
+		if err != nil {
+			http.Error(w, "Invalid Age Input", http.StatusBadRequest)
+			return
+		}
+		//add to database
+		fmt.Fprintf(w, "Successfully Received Process and will be added to database xd")
+		db.Create(&Person{Name: Name, Age: AgeInt})
+		log.Info("Added to database with Name "+Name+" and Age "+strconv.Itoa(AgeInt))
+	} else {
+		http.Error(w, "INVALID REQUEST METHOD", http.StatusBadRequest)
+	}
+}
+
+func init() {
+	// initialize logger
+	log.SetOutput(os.Stdout)
+	// connect to database
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Database failed to connect with the DSN: " + dsn)
+	}
+	// register gorm promtheus plugin
+	db.Use(gormPrometheus.New(gormPrometheus.Config{
+		DBName: "userdb",
+		RefreshInterval: 5,
+		StartServer: true,
+		HTTPServerPort: 9091,
+	}))
+	// execute the migration xd
+	db.AutoMigrate(&Person{})
+}
+
+func main() {
+	// prometheus middleware
+	mdlw := middleware.New(middleware.Config{
+		Recorder: metrics.NewRecorder(metrics.Config{}),
+	})
+	// register functions and serve requests
+	mux := http.NewServeMux()
+	mux.HandleFunc("/user/new", NewUser)
+	h := std.Handler("", mdlw, mux)
+	log.Info("start to Listen for prometheus data")
+	go func() {
+		// listen for prometheus data
+		log.Fatal(http.ListenAndServe(":9090", promhttp.Handler()).Error())
+	}()
+	// listen for new requests
+	log.Info("starting to listen for new requests at HTTP 0.0.0.0:6443")
+	log.Fatal(http.ListenAndServe(":6443", h).Error())
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/matthewzhaocc/api-cache-control
+
+go 1.15
+
+require (
+	github.com/prometheus/client_golang v1.9.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/slok/go-http-metrics v0.9.0
+	gorm.io/driver/postgres v1.0.7
+	gorm.io/gorm v1.20.12
+	gorm.io/plugin/prometheus v0.0.0-20210112035011-ae3013937adc
+)
